42d2fd4ceb3d8d41679794a66d752366f2fb15c2
Vadim <63426337+elice-qv@users.noreply.github.com>
2024-03-19 23:19:56 +0300

    update parse method
    Now you can choose a file name or search through all files

--- parser.py
+++ parser.py
@@ -9,35 +9,41 @@ def get_encoding(file_path):
     return result['encoding']
 
 
-def search_and_collect_data_chardet(search_text, file_name, root_directory, output_file='data.txt'):
-    with open(output_file, 'w', encoding='utf-8') as output:
+def search_and_collect_data_any_file(search_text, root_directory, output_file='data.txt'):
+     with open(output_file, 'w', encoding='utf-8') as output:
         for root, dirs, files in os.walk(root_directory):
-            if file_name in files:
+            for file_name in files:
                 file_path = os.path.join(root, file_name)
-                encoding = get_encoding(file_path)
-                with open(file_path, 'r', encoding=encoding, errors='replace') as f:
-                    lines = f.readlines()
-                    for i, line in enumerate(lines):
-                        if search_text.lower() in line.lower():
-                            output.write(
-                                "--------------------------------------------------------------------------------\n")
-                            output.write(f"{file_path}\n")
-                            output.write(line)
-                            for j in range(1, 4):
-                                if i + j < len(lines):
-                                    output.write(lines[i + j])
-                            output.write(
-                                "--------------------------------------------------------------------------------\n\n")
+                try:
+                    encoding = get_encoding(file_path)
+                    with open(file_path, 'r', encoding=encoding, errors='replace') as f:
+                        lines = f.readlines()
+                        for i, line in enumerate(lines):
+                            if search_text.lower() in line.lower():
+                                output.write(
+                                    "--------------------------------------------------------------------------------\n")
+                                output.write(f"{file_path}\n")
+                                output.write(line)
+                                for j in range(1, 4):
+                                    if i + j < len(lines):
+                                        output.write(lines[i + j])
+                                output.write(
+                                    "--------------------------------------------------------------------------------\n\n")
+                except Exception as e:
+                    print(f"Error processing {file_path}: {e}")
 
 
-def main_chardet():
+def main():
     search_text = input("Введите текст для поиска: ")
-    file_name = input("Введите имя файла для поиска: ")
+    file_name = input("Введите имя файла для поиска (оставьте пустым для проверки всех файлов): ")
     root_directory = input("Введите путь к корневой папке (оставьте пустым для текущей директории): ")
     root_directory = root_directory or '.'
 
-    search_and_collect_data_chardet(search_text, file_name, root_directory)
+    if file_name:
+        search_and_collect_data_any_file(search_text, root_directory, file_name)
+    else:
+        search_and_collect_data_any_file(search_text, root_directory)
 
 # Uncomment it before 'run' in IDE
-#if __name__ == "__main__":
-#    main_chardet()
+if __name__ == "__main__":
+    main()
