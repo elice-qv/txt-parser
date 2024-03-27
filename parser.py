10c434cac5f1ebb470d6b1d847e34ef719c6467a
Skele <63426337+elice-qv@users.noreply.github.com>
2024-03-27 22:57:02 +0300

    Add new feature

--- parser.py
+++ parser.py
@@ -1,49 +1,58 @@
 import os
 import chardet
 
-
 def get_encoding(file_path):
     with open(file_path, 'rb') as f:
         raw_data = f.read()
     result = chardet.detect(raw_data)
     return result['encoding']
 
+def read_file_names(file_list_path='files.txt'):
+    try:
+        with open(file_list_path, 'r', encoding='utf-8') as file:
+            file_names = [line.strip() for line in file.readlines()]
+        return file_names
+    except FileNotFoundError:
+        print(f"Файл {file_list_path} не найден. Будет выполнен поиск во всех файлах.")
+        return None
 
-def search_and_collect_data_any_file(search_text, root_directory, output_file='data.txt'):
-     with open(output_file, 'w', encoding='utf-8') as output:
+def search_in_files(search_text, file_names, root_directory, output_file='data.txt'):
+    processed_files_count = 0
+    with open(output_file, 'w', encoding='utf-8') as output:
         for root, dirs, files in os.walk(root_directory):
             for file_name in files:
-                file_path = os.path.join(root, file_name)
-                try:
-                    encoding = get_encoding(file_path)
-                    with open(file_path, 'r', encoding=encoding, errors='replace') as f:
-                        lines = f.readlines()
-                        for i, line in enumerate(lines):
-                            if search_text.lower() in line.lower():
-                                output.write(
-                                    "--------------------------------------------------------------------------------\n")
-                                output.write(f"{file_path}\n")
-                                output.write(line)
-                                for j in range(1, 4):
-                                    if i + j < len(lines):
-                                        output.write(lines[i + j])
-                                output.write(
-                                    "--------------------------------------------------------------------------------\n\n")
-                except Exception as e:
-                    print(f"Error processing {file_path}: {e}")
+                if file_names is None or file_name in file_names:
+                    file_path = os.path.join(root, file_name)
+                    try:
+                        encoding = get_encoding(file_path)
+                        with open(file_path, 'r', encoding=encoding, errors='replace') as f:
+                            lines = f.readlines()
+                            for i, line in enumerate(lines):
+                                if search_text.lower() in line.lower():
+                                    output.write("--------------------------------------------------------------------------------\n")
+                                    output.write(f"{file_path}\n")
+                                    output.write(line)
+                                    for j in range(1, 4):
+                                        if i + j < len(lines):
+                                            output.write(lines[i + j])
+                                    output.write("--------------------------------------------------------------------------------\n\n")
+                                    break  # Остановиться после первого найденного совпадения в файле
+                        processed_files_count += 1
+                        print(f"Обработано файлов: {processed_files_count}. Последний обработанный файл: {file_path}")
+                    except Exception as e:
+                        print(f"Ошибка при обработке файла {file_path}: {e}")
 
+def search_and_collect_data_interactive():
+    search_text = input("Введите ключевое слово для поиска: ")
+    root_directory = input("Введите путь к корневой папке для поиска: ")
+    file_list_path = 'files.txt'
 
-def main():
-    search_text = input("Enter the key words: ")
-    file_name = input("Enter file name (empty for choose all files): ")
-    root_directory = input("Enter root directory: ")
-    root_directory = root_directory or '.'
+    file_names = read_file_names(file_list_path)
+    if file_names is not None and len(file_names) == 0:
+        print("Список файлов для поиска пуст. Будет выполнен поиск во всех файлах.")
+        file_names = None
 
-    if file_name:
-        search_and_collect_data_any_file(search_text, root_directory, file_name)
-    else:
-        search_and_collect_data_any_file(search_text, root_directory)
+    search_in_files(search_text, file_names, root_directory)
 
-# Uncomment it before 'run' in IDE
 if __name__ == "__main__":
-    main()
+    search_and_collect_data_interactive()
